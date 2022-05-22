14b399f98e4785a879b3145df5751e8ac309fbe4
vunovikov <vunovikov@outlook.com>
2022-05-23 00:28:09 +0300

    last

--- nginx_conf.sh
+++ nginx_conf.sh
@@ -1,43 +1,58 @@
 #!/usr/bin/env bash
 
+#            systemctl cat nginx | grep -oP '(?<=ExecStart=)[^ ]+'
+#            systemctl cat nginx | awk -F'=| ' '/ExecStart/{print $2}'
+NGINX_BIN="$(systemctl cat nginx | grep -oP 'ExecStart=\K[^ ]+')"
+NGINX_CONFIG="$(systemctl cat nginx | grep -oP 'Start.* \K.*')"
+NGINX_PID="$(systemctl cat nginx | grep -oP 'PIDFile=\K.*')"
+
+NGINX_PARAMS="$(${NGINX_BIN} -V 2>&1 | grep 'con')"
+NGINX_PREFIX="$(echo "${NGINX_PARAMS}" | grep -oP 'prefix=\K[^ ]+')"
+NGINX_MODULES="$(echo "${NGINX_PARAMS}" | grep -oP 'modules-path=\K[^ ]+')"
+NGINX_ERROR_LOG="$(echo "${NGINX_PARAMS}" | grep -oP 'error-log-path=\K[^ ]+')"
+NGINX_ACCESS_LOG="$(echo "${NGINX_PARAMS}" | grep -oP 'http-log-path=\K[^ ]+')"
+NGINX_USER="$(echo "${NGINX_PARAMS}" | grep -oP 'user=\K[^ ]+')"
+NGINX_GROUP="$(echo "${NGINX_PARAMS}" | grep -oP 'group=\K[^ ]+')"
+NGINX_CC_OPT="$(echo "${NGINX_PARAMS}" | grep -oP "cc-opt='\K[^']+")"
+NGINX_LD_OPT="$(echo "${NGINX_PARAMS}" | grep -oP "ld-opt='\K[^']+")"
+
 SITE_NAME="devilindetails.com"
 EMAIL="vunovikov@outlook.com"
-NGINX_PATH="/usr/local/nginx"
+
 #PYTHON="/usr/local/bin/python3.10"
-
-#[[ -d "${NGINX_PATH}/conf" ]] && mv "${NGINX_PATH}/conf" "${NGINX_PATH}/conf.old"
-#git clone https://github.com/vunovikov/Nginx_Configuration.git "${NGINX_PATH}/conf"
-#rm -rf "${NGINX_PATH}/conf/.git"
-
 #grep -q certbot <($PYTHON -m pip list) || $PYTHON -m pip install certbot
 
-[[ -d "${NGINX_PATH}/html/${SITE_NAME}" ]] || mkdir -p "${NGINX_PATH}/html/${SITE_NAME}/_letsencrypt"
-chown -R nginx:nginx "${NGINX_PATH}/html/${SITE_NAME}"
+#[[ -d "${NGINX_PREFIX}/conf" ]] && mv "${NGINX_PREFIX}/conf" "${NGINX_PREFIX}/conf.old"
+#git clone https://github.com/vunovikov/Nginx_Configuration.git "${NGINX_PREFIX}/conf"
+#rm -rf "${NGINX_PREFIX}/conf/.git"
 
-cp "${NGINX_PATH}/conf/conf.d/example.com.conf.default" "${NGINX_PATH}/conf/conf.d/${SITE_NAME}.conf"
-sed -i -r "s/example\.com/${SITE_NAME}/g" "${NGINX_PATH}/conf/conf.d/${SITE_NAME}.conf"
+[[ -d "${NGINX_PREFIX}/html/${SITE_NAME}" ]] || mkdir -p "${NGINX_PREFIX}/html/${SITE_NAME}/_letsencrypt"
+chown -R nginx:nginx "${NGINX_PREFIX}/html/${SITE_NAME}"
+
+cp "${NGINX_PREFIX}/conf/conf.d/example.com.conf.default" "${NGINX_PREFIX}/conf/conf.d/${SITE_NAME}.conf"
+sed -i -r "s/example\.com/${SITE_NAME}/g" "${NGINX_PREFIX}/conf/conf.d/${SITE_NAME}.conf"
 
 # Сгенерируйте ключи Диффи-Хеллмана, запустив следующую команду на своем сервере:
-[[ -f "${NGINX_PATH}/conf/dhparam.pem" ]] || openssl dhparam -dsaparam -out "${NGINX_PATH}/conf/dhparam.pem" 4096
+[[ -f "${NGINX_PREFIX}/conf/dhparam.pem" ]] || openssl dhparam -dsaparam -out "${NGINX_PREFIX}/conf/dhparam.pem" 4096
 
 # Закомментируйте директивы, связанные с SSL в конфигурации:
-sed -i -r 's/(listen .*443)/\1; #/g; s/(ssl_(certificate|certificate_key|trusted_certificate) )/#;#\1/g; s/(server \{)/\1\n    ssl off;/g' "${NGINX_PATH}/conf/conf.d/${SITE_NAME}.conf"
+sed -i -r 's/(listen .*443)/\1; #/g; s/(ssl_(certificate|certificate_key|trusted_certificate) )/#;#\1/g; s/(server \{)/\1\n    ssl off;/g' "${NGINX_PREFIX}/conf/conf.d/${SITE_NAME}.conf"
 # Перезагрузите свой NGINX сервер:
-${NGINX_PATH}/sbin/nginx -t && systemctl reload nginx
+"${NGINX_PREFIX}/sbin/nginx" -t && systemctl reload nginx
 
 # Получите SSL сертификат Let's Encrypt используя Certbot:
-/usr/local/bin/certbot certonly --webroot -d "${SITE_NAME}" -d "www.${SITE_NAME}" --email "${EMAIL}" -w "${NGINX_PATH}/html/${SITE_NAME}/_letsencrypt" -n --agree-tos --force-renewal
+/usr/local/bin/certbot certonly --webroot -d "${SITE_NAME}" -d "www.${SITE_NAME}" --email "${EMAIL}" -w "${NGINX_PREFIX}/html/${SITE_NAME}/_letsencrypt" -n --agree-tos --force-renewal
 
 # Раскомментируйте директивы, связанные с SSL в конфигурации:
-sed -i -r -z 's/#?; ?#//g; s/(server \{)\n    ssl off;/\1/g' "${NGINX_PATH}/conf/conf.d/${SITE_NAME}.conf"
+sed -i -r -z 's/#?; ?#//g; s/(server \{)\n    ssl off;/\1/g' "${NGINX_PREFIX}/conf/conf.d/${SITE_NAME}.conf"
 
 # Перезагрузите свой NGINX сервер:
-${NGINX_PATH}/sbin/nginx -t && systemctl reload nginx
+"${NGINX_PREFIX}/sbin/nginx" -t && systemctl reload nginx
 
 # Настройте Certbot, чтобы перезагрузить NGINX, когда сертификаты успешно обновятся:
 {
     echo '#!/usr/bin/env bash'
-    echo "${NGINX_PATH}/sbin/nginx -t && systemctl reload nginx"
+    echo "${NGINX_PREFIX}/sbin/nginx -t && systemctl reload nginx"
 } > /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
 
 chmod a+x /etc/letsencrypt/renewal-hooks/post/nginx-reload.sh
